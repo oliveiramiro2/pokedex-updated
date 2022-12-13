4756e834647cd5128d642d98c17fdce9b20925ca
oliveiramiro2 <ramiromares95@gmail.com>
2022-12-13 08:05:06 -0300

    used function short do orderer array

--- src/Pages/public/Home/index.tsx
+++ src/Pages/public/Home/index.tsx
@@ -17,66 +17,40 @@ const aux: IPokeData[] = [];
 const HomePublic: React.FC = () => {
     const [pokeData, setPokeData] = useState<IPokeData[]>([] as IPokeData[]);
     const [, setForceRenderer] = useState<IPokeData[]>([] as IPokeData[]);
-    // const [makeRequest, setMakeRequest] = useState<boolean>(false);
     const [page] = useState<number>(1);
 
     useEffect(() => {
-        /* try {
-            if (page * 6 !== pokeData[6].id) {
-                console.log("passou");
-                setPokeData([]);
-                numberCards.map(value =>
-                    API.get(`${page * value}`).then(({ data }: any) => {
-                        setPokeData(prev => {
-                            prev.push(data);
-                            return prev;
-                        });
-                        setForceRenderer(pokeData);
-                        setForceRenderer([]);
-                    })
-                );
-            }
-        } catch {
-            if (makeRequest && pokeData.length === 0) {
-                makeRequest = false;
-
-            }
-        } */
-        if (makeRequest){
-        makeRequest = false;
-        numberCards.forEach(async value => {
-            const { data } = await API.get(`${page * value}`);
-            aux.push(await data);
-
-            console.log(aux.length, aux);
-            if (aux.length === 6) {
-                setPokeData([...aux]);
-                setForceRenderer(pokeData);
-                setForceRenderer([]);
-                console.log(pokeData);
-            }
-        });
-}
+        if (makeRequest) {
+            makeRequest = false;
+            aux.length = 0;
+            numberCards.forEach(async value => {
+                const { data } = await API.get(`${page * value}`);
+                aux.push(await data);
+                if (aux.length === 6) {
+                    aux.sort((a, b) => {
+                        if (a.id > b.id) return 1;
+                        return -1;
+                    });
+                    setPokeData([...aux]);
+                    setForceRenderer(pokeData);
+                    setForceRenderer([]);
+                }
+            });
+        }
     }, [page]);
 
     return (
         <SContainHome>
             <Header />
             <SContainAllCards>
-                {/* <SContainCard>{pokeData?.name}</SContainCard>
-                <SContainCard>{pokeData?.name}</SContainCard>
-                <SContainCard>{pokeData?.name}</SContainCard>
-                <SContainCard>{pokeData?.name}</SContainCard>
-                <SContainCard>{pokeData?.name}</SContainCard>
-                <SContainCard>{pokeData?.name}</SContainCard> */}
-                {pokeData.length > 1 ? (
+                {pokeData.length === 6 ? (
                     pokeData.map(value => (
                         <SContainCard key={value.id}>
                             <p>{value?.name}</p>
                         </SContainCard>
                     ))
                 ) : (
-                    <div>aaaaaaaa</div>
+                    <div>wait</div>
                 )}
             </SContainAllCards>
         </SContainHome>
